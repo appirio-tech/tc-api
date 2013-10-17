08c54572c87112bbd5b75d6c89cf706379efe000
djh <djh@dev.(none)>
2013-10-17 17:03:15 +0000

    use dev TC oauth provider

--- config.js
+++ config.js
@@ -13,10 +13,10 @@
  */
 
 var config = {
-    clientID: "a6d12f56",
-    clientSecret: "b4f8b6080da88e76f902ad8bd9fc9c56",
+    clientID: "0af315c5",
+    clientSecret: "766a21fe4c8e6d2e332983471ace9902",
     callbackURL: "http://192.241.132.180:8080/topcoderoauth/callback",
-    apiHost: "107.21.158.122",
+    apiHost: "107.21.158.122", //"api.topcoder.com",//
     scope: ["FORUMS_REST", "CONTEST_REST"]
 };
 
