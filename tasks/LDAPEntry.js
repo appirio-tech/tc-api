c111594c2bcf245cb8aab0920d007e05de6cacf2
skyhit <skyhit@debian.debian>
2013-11-22 09:36:53 -0500

    Action Tasks for Adding LDAP Entry and Send Activiation emails

---
+++ tasks/LDAPEntry.js
@@ -0,0 +1,254 @@
+/*
+ * Copyright (C) 2013 TopCoder Inc., All Rights Reserved.
+ *
+ * Version: 1.0
+ * Author: TCSASSEMBLER
+ */
+"use strict";
+
+/**
+ * Dependency 
+ */
+var ldap = require('ldapjs'), async = require('async');
+
+/// environment variables
+var ldap_host = process.env.TC_LDAP_HOST,
+    ldap_host_port = process.env.TC_LDAP_PORT,
+    ldap_password = process.env.TC_LDAP_PASSWORD,
+    ldap_host_bind_dn = process.env.TC_BIND_DN,
+    topcoder_member_base_dn = process.env.TC_LDAP_MEMBER_BASE_DN,
+    topcoder_member_active = 'A',
+    topcoder_member_unactive = 'U';
+
+/**
+ * This function is used to translate the error of LADP
+ * into a form of normal object
+ * 
+ * @param {Object} err - the error object returned by ldapjs
+ * @param {Object} normal object of error
+ */
+var translateLdapError = function (err) {
+    if (err) {
+        return {
+            dn: err.dn,
+            code: err.code,
+            name: err.name,
+            message: err.message
+        };
+    }
+    return {};
+};
+
+/**
+ * This function is used to check the existence of 
+ * given paramters and ensure it not be empty
+ * 
+ * @param {Object} params - a object of paramters
+ * @param {String} name - the name of the to-be-checked parameter
+ * @param {Boolean} true if params contains the given paramter 
+ *                      and it is not empty; false otherwise. 
+ */
+var checkParameter = function (params, name) {
+    return params.hasOwnProperty(name) && (params[name].toString().trim().length >= 1);
+};
+
+/**
+ * Function used to create a client
+ */
+var createClient = function () {
+    return ldap.createClient({
+        url: 'ldaps://' + ldap_host + ':' + ldap_host_port,
+        tlsOptions: {
+            rejectUnauthorized: false
+        }
+    });
+};
+
+/**
+ * Function used to bind a ldap server
+ * 
+ * @param {Object} api - object used to access infrustrature
+ * @param {Object} client - an object of current client of ldap server
+ * @param {Function} callback - a async callback funtion with prototype like callback(err, results)
+ */
+var bindClient = function (api, client, callback) {
+    client.bind(ldap_host_bind_dn, ldap_password, function (err) {
+        if (err) {
+            callback('cannot bind to ldap server', translateLdapError(err));
+        } else {
+            api.log('Sucessfully bind to ldap server', 'info');
+            callback(null, 'bind to ldap server');
+        }
+    });
+};
+
+/**
+ * Function used to add an entry in ldap server
+ * 
+ * @param {Object} api - object used to access infrustrature
+ * @param {Object} client - an object of current client of ldap server
+ * @param {Object} params - the parameters of task
+ * @param {Function} callback - a async callback funtion with prototype like callback(err, results)
+ */
+var addClient = function (api, client, params, callback) {
+    var dn = 'uid=' + params.userId + ', ' + topcoder_member_base_dn,
+        entry = {
+            uid: params.userId,
+            handle: params.handle,
+            objectClass: ['tc-member', 'top'],
+            status: topcoder_member_unactive,
+            userPassword: params.password
+        };
+    client.add(dn, entry, function (err) {
+        if (err) {
+            client.unbind();
+            callback('cannot add to ldap server', translateLdapError(err));
+        } else {
+            api.log('Sucessfully add to ldap server', 'info');
+            callback(null, 'add to ldap server');
+        }
+    });
+};
+
+/**
+ * Function used to modify an entry in ldap server
+ * 
+ * @param {Object} api - object used to access infrustrature
+ * @param {Object} client - an object of current client of ldap server
+ * @param {Object} params - the parameters of task
+ * @param {Function} callback - a async callback funtion with prototype like callback(err, results)
+ */
+var modifyClient = function (api, client, params, callback) {
+    var dn = 'uid=' + params.userId + ', ' + topcoder_member_base_dn,
+        change = new ldap.Change({
+            operation: 'replace',
+            modification: {
+                status: [topcoder_member_active]
+            }
+        });
+    client.modify(dn, change, function (err) {
+        if (err) {
+            client.unbind();
+            callback('cannot modify to ldap server', translateLdapError(err));
+        } else {
+            api.log('Sucessfully modify to ldap server', 'info');
+            callback(null, 'modify to ldap server');
+        }
+    });
+};
+
+/**
+ * Task - used to add a ldap entry 
+ */
+exports.addMemberProfileLDAPEntry = {
+    name: 'addMemberProfileLDAPEntry',
+    description: 'I will add an ldap entry to ldap server',
+    scope: 'any',
+    frequency: 0,
+    /**
+     * Main function of addMemberProfileLDAPEntry tasks
+     *
+     * @param {Object} api - object used to access infrustrature
+     * @param {Object} params require fields: userId, handle, password, errorHandler(err) for error handle
+     * @param {Function} next - callback function
+     */
+    run: function (api, params, next) {
+        api.log('[task @ addMemberProfileLDAPEntry] Enter addMemberProfileLDAPEntry task', 'debug');
+
+        var client, error, index, requiredParams = ['userId', 'handle', 'password'];
+
+        // pararms validation
+        if ((!params.hasOwnProperty('errorHandler')) || (typeof params.errorHandler !== 'function')) {
+            api.log('[task warning @ addMemberProfileLDAPEntry] No error handler assigned', 'warning');
+            params.errorHandler = function (err) { console.log(err); };
+        }
+        for (index = 0; index < requiredParams.length; index += 1) {
+            if (!checkParameter(params, requiredParams[index])) {
+                api.log('[ task err @ addMemberProfileLDAPEntry] parameter <' + requiredParams[index] + '> missing', 'error');
+                params.errorHandler({ message: 'required parameter <' + requiredParams[index] + '> missing' });
+                return next(null, true);
+            }
+        }
+
+        async.series([
+            function (callback) {
+                client  = createClient();
+                callback(null, 'create client');
+            },
+            function (callback) {
+                bindClient(api, client, callback);
+            },
+            function (callback) {
+                addClient(api, client, params, callback);
+            }
+        ], function (err, result) {
+            if (err) {
+                error = result.pop();
+                api.log('[task error @ addMemberProfileLDAPEntry] ' + err + ' ', 'error', error);
+                params.errorHandler(error);
+            } else {
+                client.unbind();
+            }
+            api.log('[task @ addMemberProfileLDAPEntry] Leave addMemberProfileLDAPEntry task', 'debug');
+        });
+        return next(null, true);
+    }
+};
+
+/**
+ * Task - used to activate a ldap entry 
+ */
+exports.activateMemberProfileLDAPEntry = {
+    name: 'activateMemberProfileLDAPEntry',
+    description: 'I will activate an ldap entry to ldap server',
+    scope: 'any',
+    frequency: 0,
+
+    /**
+     * Main function of activateMemberProfileLDAPEntry tasks
+     *
+     * @param {Object} api - object used to access infrustrature
+     * @param {Object} params require fields: userId, errorHandler(err) for error handle
+     * @param {Function} next - callback function
+     */
+    run: function (api, params, next) {
+        api.log('[task @ activateMemberProfileLDAPEntry] Enter activateMemberProfileLDAPEntry task', 'debug');
+        var client, error;
+
+        // pararms validation
+        if ((!params.hasOwnProperty('errorHandler')) || (typeof params.errorHandler !== 'function')) {
+            api.log('[task warning @ activateMemberProfileLDAPEntry] No error handler assigned', 'warning');
+            params.errorHandler = function (err) { console.log(err); };
+        }
+
+        if (!checkParameter(params, 'userId')) {
+            api.log('[task err @ activateMemberProfileLDAPEntry] required parameter <userId> missing', 'error');
+            params.errorHandler({ message: 'required parameter <userId> missing' });
+            return next(null, true);
+        }
+
+        async.series([
+            function (callback) {
+                client = createClient();
+                callback(null, 'create client');
+            },
+            function (callback) {
+                bindClient(api, client, callback);
+            },
+            function (callback) {
+                modifyClient(api, client, params, callback);
+            }
+        ], function (err, result) {
+            if (err) {
+                error = result.pop();
+                api.log('[task error @ activateMemberProfileLDAPEntry] ' + err + ' ', 'error', error);
+                params.errorHandler(error);
+            } else {
+                client.unbind();
+            }
+            api.log('[task @ activateMemberProfileLDAPEntry] Leave activateMemberProfileLDAPEntry task', 'debug');
+        });
+        return next(null, true);
+    }
+};
+
---
+++ tasks/sendActivationEmail.js
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2013 TopCoder Inc., All Rights Reserved.
+ *
+ * Version: 1.0
+ * Author: TCSASSEMBLER
+ */
+"use strict";
+
+/**
+ * Dependency 
+ */
+var nodemailer = require('nodemailer'),
+    path = require('path'),
+    templatesDir = path.join(__dirname, '../' + process.env.TC_EMAIL_TEMPLATE_DIR),
+    emailTemplates = require('email-templates');
+
+/// environment variables
+var tc_email_account = process.env.TC_EMAIL_ACCOUNT,
+    tc_email_password = process.env.TC_EMAIL_PASSWORD,
+    tc_email_host = process.env.TC_EMAIL_HOST,
+    tc_email_host_port = process.env.TC_EMAIL_HOST_PORT;
+
+/**
+ * This function is used to check the existence of 
+ * given paramters and ensure it not be empty
+ * 
+ * @param {Object} params - a object of paramters
+ * @param {String} name - the name of the to-be-checked parameter
+ * @param {Boolean} true if params contains the given paramter 
+ *                      and it is not empty; false otherwise. 
+ */
+var checkParameter = function (params, name) {
+    return params.hasOwnProperty(name) && (params[name].toString().trim().length >= 1);
+};
+
+
+/**
+ * Task - used to send a verification email 
+ */
+var sendActivationEmail = {
+    name: 'sendActivationEmail',
+    description: 'I will send activation Email',
+    scope: 'any',
+    frequency: 0,
+    /**
+     * Main function of addLdapEntry tasks
+     *
+     * @param {Object} api - object used to access infrustrature
+     * @param {Object} params require fields: subject, activationCode, 
+     *                          template, toAddress, fromAddress,
+     *                          senderName, url,
+     *                          errorHandler(err) for error handle
+     * @param {Function} next - callback function
+     */
+    run: function (api, params, next) {
+        api.log('[task @ sendActivationEmail] Enter sendActivationEmail task', 'info');
+        var index, transport, locals, message, requiredParams = ['subject', 'activationCode',
+            'template', 'toAddress', 'fromAddress', 'senderName', 'url'];
+
+        // validation parameter
+        if ((!params.hasOwnProperty('errorHandler')) || (typeof params.errorHandler !== 'function')) {
+            api.log('[task warning @ addLdapEntry] No error handler assigned', 'warning');
+            params.errorHandler = function (err) { console.log(err); };
+        }
+
+        for (index = 0; index < requiredParams.length; index += 1) {
+            if (!checkParameter(params, requiredParams[index])) {
+                api.log('[ task err @ sendActivationEmail] parameter <' +
+                    requiredParams[index] + '> missing', 'error');
+                params.errorHandler({ message: 'required parameter <' + requiredParams[index] + '> missing' });
+                return next(null, true);
+            }
+        }
+
+        // build email from templates
+        emailTemplates(templatesDir, function (err, template) {
+            if (err) { // fail to read templates directory
+                api.log('[ task err @ sendActivationEmail] failed to get templates directory',
+                    'error', {err: JSON.stringify(err), dir: templatesDir});
+                params.errorHandler(err);
+                return;
+            }
+
+            // local variables in email
+            locals = {
+                activationCode: params.activationCode,
+                url: params.url
+            };
+
+            // build email from given template
+            template(params.template, locals, function (err, html, text) {
+                if (err) { // unable to locate the assigned template
+                    api.log('[ task err @ sendActivationEmail] failed to get template',
+                        'error', {err: JSON.stringify(err)});
+                    params.errorHandler(err);
+                    return;
+                }
+
+                // create transport for activation email
+                transport = nodemailer.createTransport("SMTP", {
+                    host: tc_email_host,
+                    port: tc_email_host_port,
+                    secureConnection: true,
+                    requiresAuth: true,
+                    auth: {
+                        user: tc_email_account,
+                        pass: tc_email_password
+                    }
+                });
+
+                // build email message
+                message = {
+                    from: params.fromAddress,
+                    to: params.toAddress,
+                    subject: params.subject,
+                    html: html,
+                    text: text
+                };
+
+                // send email
+                transport.sendMail(message, function (err) {
+                    if (err) { // unable to send email
+                        api.log('[ task err @ sendActivationEmail] cannot send email', 'error', {
+                            err: JSON.stringify(err)
+                        });
+                        params.errorHandler(err);
+                        return;
+                    }
+                    api.log('[ task @ sendActivationEmail] activation email sent', 'info');
+                    // close transport in the end.
+                    transport.close();
+                });
+            });
+        });
+        api.log('[task @ sendActivationEmail] Leave sendActivationEmail task', 'info');
+        return next(null, true);
+    }
+};
+
+/////////////////////////////////////////////////////////////////////
+// exports
+exports.task = sendActivationEmail;
+
