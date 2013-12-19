ec7e8f997263e97bcf10bb7316ed1f89fb06486b
ben <ben@ben.local>
2013-12-19 17:07:52 +0800

    debug

--- test/test.contests.js
+++ test/test.contests.js
@@ -54,9 +54,10 @@ describe('Test Contests API', function () {
                     .concat(generatePath("software-active-contests", 4))
                     .concat(generatePath("software-past-contests", 4))
                     .concat(generatePath("software-upcoming-contests", 4))
-                    /*
+
                     .concat(generatePath("software-active-private", 1))
-                    .concat(generatePath("software-past-private", 1))
+                    .concat(generatePath("software-past-private", 1));
+                    /*
                     .concat(generatePath("software-upcoming-private", 1))
                     .concat(generatePath("software-details-contests", 1));
                     */;
