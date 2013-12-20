ce13eef6d66ee358eec416704830fb04ac548fb7
ben <ben@ben.local>
2013-12-20 19:23:17 +0800

    fixed test data and removed rank type missing test for topsSRM and topsMarathon

--- test/test.topsMarathon.js
+++ test/test.topsMarathon.js
@@ -157,27 +157,6 @@ describe('Get Top Ranked Marathon Members API', function () {
         assertResponse("rankType=competitors&pageIndex=-1", "expected_top_marathon_7", done);
     });
 
-
-    /**
-     * /v2/data/marathon/statistics/tops
-     */
-    it("should return error when rankType is missing", function (done) {
-        request(API_ENDPOINT)
-            .get('/v2/data/marathon/statistics/tops')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200) //action hero set always code 200 if requred parameter is missing
-            .end(function (err, res) {
-                if (err) {
-                    done(err);
-                    return;
-                }
-                assert.equal(res.body.error, "Error: rankType is a required parameter for this action");
-                done();
-            });
-    });
-
-
     /**
      * /v2/data/marathon/statistics/tops?rankType=xyz
      */
