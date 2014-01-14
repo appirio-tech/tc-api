5f52ea3b57f64a340ad45bcc0775cb92e8ba4742
delemach <delemach@gmail.com>
2014-01-14 12:36:58 +0000

    fixed idGenerator to actually work as intended

--- initializers/idGenerator.js
+++ initializers/idGenerator.js
@@ -20,6 +20,8 @@ var idBlockSize = 10;
  */
 exports.idGenerator = function (api, next) {
     api.idGenerator = {
+        nextID : {},
+        maxID : {},
         /**
          * Get the next value of an id sequence.
          * The result will be passed to the "next" callback. It is a number representing the next id value.
@@ -29,31 +31,40 @@ exports.idGenerator = function (api, next) {
          * @param {Function} next - The callback function
          */
         getNextID : function (idName, dbConnectionMap, next) {
-            api.dataAccess.executeQuery("get_highest_id", {name : idName}, dbConnectionMap, function (err, result) {
-                api.log("Execute result returned", "debug");
-                if (err) {
-                    api.log("Error occured: " + err + " " + (err.stack || ''), "error");
-                } else {
-                    api.log("Forward result", "debug");
-                }
+            if (!api.idGenerator.nextID[idName] || !api.idGenerator.maxID[idName] || api.idGenerator.nextID[idName] >= api.idGenerator.maxID[idName]) {
 
-                var highest = result[0].highest;
-                api.log("Current highest id for " + idName + " : " + highest, "debug");
-
-                highest += idBlockSize;
-
-                api.dataAccess.executeQuery("update_highest_id", {nextBlockStart : highest, name : idName}, dbConnectionMap, function (err, result) {
+                api.dataAccess.executeQuery("get_highest_id", {name : idName}, dbConnectionMap, function (err, result) {
                     api.log("Execute result returned", "debug");
                     if (err) {
                         api.log("Error occured: " + err + " " + (err.stack || ''), "error");
-                    } else {
-                        api.log("Forward result", "debug");
+                        next(err);
+                        return;
                     }
 
-                    api.log("Updated result: " + result, "debug");
-                    next(err, highest);
+                    var highest = result[0].highest;
+                    api.log("Current highest id for " + idName + " : " + highest, "debug");
+
+                    api.idGenerator.nextID[idName] = highest + 1;
+                    highest += idBlockSize;
+                    api.idGenerator.maxID[idName] = highest;
+
+                    api.dataAccess.executeQuery("update_highest_id", {nextBlockStart : highest, name : idName}, dbConnectionMap, function (err, result) {
+                        api.log("Execute result returned", "debug");
+                        if (err) {
+                            api.log("Error occured: " + err + " " + (err.stack || ''), "error");
+                            next(err);
+                            return;
+                        }
+                        api.log("Updated result: " + result, "debug");
+                        next(null, api.idGenerator.nextID[idName]);
+                    });
                 });
-            });
+
+            //we have id's reserved already, so just increment and use it. Note we purposely skip the block start/end as ids to avoid edge cases.
+            } else {
+                api.idGenerator.nextID[idName] = api.idGenerator.nextID[idName] + 1;
+                next(null, api.idGenerator.nextID[idName]);
+            }
         }
     };
     next();
