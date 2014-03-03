852f13e63de67e1f2e024dc42040287160ca4356
ecnu_haozi <accept.acm@gmail.com>
2014-03-03 12:06:28 +0800

    Add register challenge api

--- actions/memberRegistration.js
+++ actions/memberRegistration.js
@@ -296,7 +296,7 @@ var registerUser = function (user, api, dbConnectionMap, next) {
                     url = process.env.TC_ACTIVATION_SERVER_NAME + '/reg2/activate.action?code=' + activationCode;
                     api.log("Activation url: " + url, "debug");
 
-                    api.tasks.enqueue("sendActivationEmail", {subject : activationEmailSubject, activationCode : activationCode, template : 'activation_email', toAddress : user.email, fromAddress : process.env.TC_EMAIL_ACCOUNT, senderName : activationEmailSenderName, url : url}, 'default');
+                    api.tasks.enqueue("sendEmail", {subject : activationEmailSubject, activationCode : activationCode, template : 'activation_email', toAddress : user.email, fromAddress : process.env.TC_EMAIL_ACCOUNT, senderName : activationEmailSenderName, url : url}, 'default');
                         
                     callback(null, null);
                     
