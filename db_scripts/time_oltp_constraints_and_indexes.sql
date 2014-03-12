5c892f03ed3113a1dc312705a6419298341b688b
iRabbit <xing.wenhui@gmail.com>
2014-03-13 00:45:16 +0800

    remove db_scripts folder

--- db_scripts/time_oltp_constraints_and_indexes.sql
+++
@@ -1,2 +0,0 @@
-database time_oltp;
--- ALTER TABLE project ADD constraint unique (subscription_number) constraint unq_proj_subscription_numb;
--- db_scripts/time_oltp_main_schema.sql
+++
@@ -1,2 +0,0 @@
-database time_oltp;
-ALTER TABLE project ADD subscription_number varchar(64);
