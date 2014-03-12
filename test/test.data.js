c741828e8c2c420f4db5dc475ca427e849520024
Sung Hyun Kim <muzehyun@gmail.com>
2014-03-12 12:59:16 +0900

    Platforms and Technologies API - allowed anonymous, return all existing platforms and technologies

--- test/test.data.js
+++ test/test.data.js
@@ -1,8 +1,10 @@
 /*
  * Copyright (C) 2014 TopCoder Inc., All Rights Reserved.
  *
- * @version 1.0
- * @author TCSASSEMBER
+ * @version 1.1
+ * @author muzehyun
+ * @changes in 1.1
+ * - Allowed anonymous user, fixed expected result
  */
 "use strict";
 /*global describe, it, before, beforeEach, after, afterEach, __dirname */
@@ -32,41 +34,6 @@ describe('', function () {
     beforeEach(function (done) {
         memberHeader = "Bearer " + testHelper.getMemberJwt();
         done();
-    });
-
-    /**
-     * Clear database
-     * @param {Function<err>} done the callback
-     */
-    function clearDb(done) {
-        async.waterfall([
-            function (cb) {
-                testHelper.runSqlFile(SQL_DIR + "tcs_catalog__clean", "tcs_catalog", cb);
-            }
-        ], done);
-    }
-
-    /**
-     * This function is run before all tests.
-     * Generate tests data.
-     * @param {Function<err>} done the callback
-     */
-    before(function (done) {
-        async.waterfall([
-            clearDb,
-            function (cb) {
-                testHelper.runSqlFile(SQL_DIR + "tcs_catalog__insert_test_data", "tcs_catalog", cb);
-            }
-        ], done);
-    });
-
-    /**
-     * This function is run after all tests.
-     * Clean up all data.
-     * @param {Function<err>} done the callback
-     */
-    after(function (done) {
-        clearDb(done);
     });
 
     /**
@@ -137,36 +104,142 @@ describe('', function () {
     }
 
     describe('Test Platforms API', function () {
+        
+        var expected = ["AWS",
+                        "Beanstalk",
+                        "Box",
+                        "Brivo Labs",
+                        "Cloud Foundry",
+                        "CloudFactor",
+                        "DocuSign",
+                        "EC2",
+                        "Facebook",
+                        "FinancialForce",
+                        "Force.com",
+                        "Gaming",
+                        "Google",
+                        "HTML",
+                        "Heroku",
+                        "MESH01",
+                        "Microsoft Azure",
+                        "Mobile",
+                        "NodeJS",
+                        "Other",
+                        "Salesforce.com",
+                        "Smartsheet",
+                        "Twilio",
+                        "Wordpress",
+                        "iOS"];
+
         /**
          * It should return 401 error for anonymouse user
          */
-        it("should return 401 error for anonymouse user", function (done) {
-            assertErrorResponse('platforms', 401, null, null, done);
+        it("should return 200 success for anonymouse user and valid platforms data", function (done) {
+            assertResponse('platforms', null, 'platforms', expected, done);
         });
 
         /**
          * It should return 200 success for any user and valid platforms data
          */
         it("should return 200 success for any user and valid platforms data", function (done) {
-            assertResponse('platforms', memberHeader, 'platforms',
-                ["AWS", "Cloud Foundry", "Google", "Heroku", "Salesforce.com"], done);
+            assertResponse('platforms', memberHeader, 'platforms', expected, done);
         });
     });
 
     describe('Test Technologies API', function () {
+        
+        var expected = [".NET",
+                        ".NET 2.0",
+                        ".NET 3.0",
+                        ".NET 3.5",
+                        ".NET System.Addins",
+                        "ADO.NET",
+                        "AJAX",
+                        "ASP.NET",
+                        "ASP.NET AJAX",
+                        "ActionScript",
+                        "Active Directory",
+                        "Apache Derby",
+                        "Applet",
+                        "C",
+                        "C#",
+                        "C++",
+                        "COM",
+                        "COM+",
+                        "CSS",
+                        "Castor",
+                        "ClickOnce",
+                        "Custom Tag",
+                        "Dojo",
+                        "EJB",
+                        "EJB 3",
+                        "Eclipse Plugin",
+                        "Flash",
+                        "Flex",
+                        "HTML",
+                        "HTTP",
+                        "Hibernate",
+                        "IIS",
+                        "J2EE",
+                        "J2ME",
+                        "JBoss Seam",
+                        "JDBC",
+                        "JFace",
+                        "JMS",
+                        "JPA",
+                        "JSF",
+                        "JSON",
+                        "JSP",
+                        "JUnit",
+                        "Java",
+                        "Java Application",
+                        "JavaBean",
+                        "JavaScript",
+                        "LDAP",
+                        "MIDP 2.0",
+                        "MSMQ",
+                        "Microsoft SilverLight",
+                        "MySQL",
+                        "Objective C",
+                        "Oracle 10g",
+                        "Oracle 9i",
+                        "PHP",
+                        "PostgreSQL",
+                        "RMI",
+                        "Remoting",
+                        "Ruby",
+                        "SQL Server 2000",
+                        "SQL Server 2005",
+                        "SWT",
+                        "Servlet",
+                        "Spring",
+                        "Struts",
+                        "Swing",
+                        "VB",
+                        "VB.NET",
+                        "WPF",
+                        "Web Services",
+                        "WinForms Controls",
+                        "Windows Communication Foundation",
+                        "Windows Server 2003",
+                        "Windows Workflow Foundation",
+                        "XAML",
+                        "XML",
+                        "XSL",
+                        "XUL"];
+
         /**
          * It should return 401 error for anonymouse user
          */
-        it("should return 401 error for anonymouse user", function (done) {
-            assertErrorResponse('technologies', 401, null, null, done);
+        it("should return 200 success for anonymouse user and valid technologies data", function (done) {
+            assertResponse('technologies', null, 'technologies', expected, done);
         });
 
         /**
          * It should return 200 success for any user and valid technologies data
          */
         it("should return 200 success for any user and valid technologies data", function (done) {
-            assertResponse('technologies', memberHeader, 'technologies',
-                ["EJB", "J2EE", "JSP", "Java", "JavaBean", "Servlet"], done);
+            assertResponse('technologies', memberHeader, 'technologies', expected, done);
         });
     });
 });
