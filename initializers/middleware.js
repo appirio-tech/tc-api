c74f708778164da366e7c0be3b6d183700ef56e4
mashannon168 <sma@appirio.com>
2014-03-18 20:38:04 -0500

    Commented throttle for now.

--- initializers/middleware.js
+++ initializers/middleware.js
@@ -344,9 +344,9 @@ exports.middleware = function (api, next) {
     }
 
     api.actions.preProcessors.push(authorizationPreProcessor);
-    api.actions.preProcessors.push(preThrottleProcessor);
+    //api.actions.preProcessors.push(preThrottleProcessor);
     api.actions.preProcessors.push(preCacheProcessor);
     api.actions.postProcessors.push(postCacheProcessor);
-    api.actions.postProcessors.push(postThrottleProcessor);
+    //api.actions.postProcessors.push(postThrottleProcessor);
     next();
 };
