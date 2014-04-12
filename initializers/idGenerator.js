ce278cabd3b9f6bb7d3a915864f7ea74167fdcf1
Ghost141 <kidd747@gmail.com>
2014-04-12 21:14:51 +0800

    Update getNextIdFromDb method in initializers/idGenerator.js.

--- initializers/idGenerator.js
+++ initializers/idGenerator.js
@@ -11,6 +11,22 @@
 
 "use strict";
 var async = require('async');
+var fs = require('fs');
+
+/**
+ * The dir that store all the query used by tc api.
+ */
+var dir = './queries';
+/**
+ * The get next sequence query.
+ */
+var GET_NEXT_SEQUENCE = null;
+/**
+ * Get the query content from the file.
+ */
+fs.readFile(dir + '/get_next_sequence', 'utf8', function (err, sql) {
+    GET_NEXT_SEQUENCE = sql;
+});
 
 /**
  * Expose the "idGenerator" utility.
@@ -51,15 +67,49 @@ exports.idGenerator = function (api, next) {
          * @since 1.2
          */
         getNextIDFromDb : function (idName, dbName, dbConnectionMap, next) {
-            api.log("Generate next id for sequence: " + idName + " in database: " + dbName, "debug");
-            api.dataAccess.executeQuery("get_next_sequence_" + dbName, {seq_name : idName}, dbConnectionMap, function (err, result) {
-                if (err) {
-                    api.log(err.message + "\n" + err.stack, "error");
-                    next(err);
-                    return;
-                }
-                next(null, result[0].next_id);
-            });
+//            api.log("Generate next id for sequence: " + idName + " in database: " + dbName, "debug");
+//            api.dataAccess.executeQuery("get_next_sequence_" + dbName, {seq_name : idName}, dbConnectionMap, function (err, result) {
+//                if (err) {
+//                    api.log(err.message + "\n" + err.stack, "error");
+//                    next(err);
+//                    return;
+//                }
+//                next(null, result[0].next_id);
+//            });
+
+            api.log("Generate next id for sequence:" + idName + " in database: " + dbName, "debug");
+            var connection = dbConnectionMap[dbName],
+                error = api.helper.checkObject(connection, 'connection');
+            if (error) {
+                next(error);
+                return;
+            }
+
+            if (connection.isConnected()) {
+                async.waterfall([
+                    function (cb) {
+                        api.dataAccess._parameterizeQuery(GET_NEXT_SEQUENCE, { seq_name: idName }, cb);
+                    },
+                    function (query, cb) {
+                        connection.query(query, cb, {
+                            start: function (q) {
+                                api.log('Start to execute ' + q, 'debug');
+                            },
+                            finish: function (f) {
+                                api.log('Finish executing ' + f, 'debug');
+                            }
+                        }).execute();
+                    }
+                ], function (err, result) {
+                    if (err) {
+                        api.log(err.message + "\n" + err.stack, "error");
+                        next(err);
+                        return;
+                    }
+                    next(null, result[0].next_id);
+                });
+            }
+
         }
     };
     next();
