ad2e5b6319e1aeb67fbf409d7e4a9c2a62cc19ef
jamesdotcom <james@appirio.com>
2014-04-30 13:02:59 -0400

    modified start

--- start-cluster.sh
+++ start-cluster.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # nohup node ~/tc-api/node_modules/.bin/actionHero start 2>&1 &
-forever start -d -v -a -l "${DIR}/log/forever.log" "${DIR}/node_modules/actionhero/bin/actionhero" startCluster --workers=1
+"${DIR}/node_modules/actionhero/bin/actionhero" startCluster --workers=1 --daemon
