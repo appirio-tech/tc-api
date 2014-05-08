4b041f0b2eab032882928f33e3efbca5e82d19f8
Ghost141 <kidd747@gmail.com>
2014-05-08 12:50:13 +0800

    Fix error in update user profile test.

--- test/test.updateUserProfile.js
+++ test/test.updateUserProfile.js
@@ -34,15 +34,14 @@ describe('Test update user profile. ', function () {
     /**
      * Users that we have setup.
      */
-    var user132456 = 'ad|132456';
+    var user124916 = 'ad|124916';
 
     /**
      * Return the authentication header to be used for the given user.
      * @param {Object} user the user to authenticate
      */
     function getAuthHeader(user) {
-        var authHeader = "Bearer " + jwt.sign({sub: user}, SECRET, {expiresInMinutes: 1000, audience: CLIENT_ID});
-        return authHeader;
+        return "Bearer " + jwt.sign({sub: user}, SECRET, {expiresInMinutes: 1000, audience: CLIENT_ID});
     }
 
     /**
@@ -54,13 +53,12 @@ describe('Test update user profile. ', function () {
      * @param {Number} expectedStatusCode the expected status code of the response
      */
     function getRequest(url, user, expectedStatusCode) {
-        var req = request(API_ENDPOINT)
+        return request(API_ENDPOINT)
             .post(url)
             .set('Accept', 'application/json')
             .set('Authorization', getAuthHeader(user))
             .expect('Content-Type', /json/)
             .expect(expectedStatusCode);
-        return req;
     }
 
 
@@ -123,9 +121,9 @@ describe('Test update user profile. ', function () {
      * Test update quote.
      */
     it('Test update quote', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ quote: 'new 1'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -134,7 +132,7 @@ describe('Test update user profile. ', function () {
      * Test update quote with long value.
      */
     it('Test update quote', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ quote: '12345678901234567890123456789012345678901234567890123456789012345678901234567890' +
             '12345678901234567890123456789012345678901234567890123456789012345678901234567890' +
             '12345678901234567890123456789012345678901234567890123456789012345678901234567890' +
@@ -148,9 +146,9 @@ describe('Test update user profile. ', function () {
      * Test update emailNotification.
      */
     it('Test update emailNotification', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ emailNotification: '{ "Algorithm Competitions": "Yes", "Software Development Opportunities": "Yes"}'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -159,7 +157,7 @@ describe('Test update user profile. ', function () {
      * Test update emailNotification with invalid data.
      */
     it('Test update emailNotification', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ emailNotification: '{ "Unknown": "Yes"}'})
             .end(function (err, resp) {
                 assertError(err, resp, "Unknown is invalid key.", done);
@@ -170,7 +168,7 @@ describe('Test update user profile. ', function () {
      * Test update country with invalid data.
      */
     it('Test update country', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ country: 'Unknown'})
             .end(function (err, resp) {
                 assertError(err, resp, "Unknown is not a valid country name.", done);
@@ -181,9 +179,9 @@ describe('Test update user profile. ', function () {
      * Test update country.
      */
     it('Test update country', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ country: 'China'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -192,9 +190,9 @@ describe('Test update user profile. ', function () {
      * Test update age.
      */
     it('Test update age', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ age: '25 - 34'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -203,7 +201,7 @@ describe('Test update user profile. ', function () {
      * Test update age with invalid data.
      */
     it('Test update age', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ age: '25 - 25'})
             .end(function (err, resp) {
                 assertError(err, resp, "25 - 25 is not a valid age value.", done);
@@ -214,9 +212,9 @@ describe('Test update user profile. ', function () {
      * Test update gender.
      */
     it('Test update gender', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ gender: 'Male'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -225,7 +223,7 @@ describe('Test update user profile. ', function () {
      * Test update gender with invalid data.
      */
     it('Test update gender', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ gender: 'aa'})
             .end(function (err, resp) {
                 assertError(err, resp, "aa is not a valid gender value.", done);
@@ -236,9 +234,9 @@ describe('Test update user profile. ', function () {
      * Test update shirtSize
      */
     it('Test update shirtSize', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ shirtSize: 'Medium'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -247,7 +245,7 @@ describe('Test update user profile. ', function () {
      * Test update shirtSize with invalid data.
      */
     it('Test update shirtSize', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ shirtSize: 'aa'})
             .end(function (err, resp) {
                 assertError(err, resp, "aa is not a valid shirtSize value.", done);
@@ -258,9 +256,9 @@ describe('Test update user profile. ', function () {
      * Test update emails
      */
     it('Test update emails', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ emails: '[{ "email": "foo2@fooonyou.com", "type": "Primary" }]'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -269,7 +267,7 @@ describe('Test update user profile. ', function () {
      * Test update emails with invalid data.
      */
     it('Test update emails', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ emails: '[{ "email": "aa", "type": "Primary", "status": "Active" }]'})
             .end(function (err, resp) {
                 assertError(err, resp, "Primary email should be email address.", done);
@@ -280,9 +278,9 @@ describe('Test update user profile. ', function () {
      * Test update address.
      */
     it('Test update address', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ address: '{ "address1" : "address1", "address2" : "address2", "address3" : "address3", "city" : "city", "zip" : "111222"}'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -291,9 +289,9 @@ describe('Test update user profile. ', function () {
      * Test update messageBlackList.
      */
     it('Test update messageBlackList', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ messageBlackList: '["Hung", "user"]'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -302,7 +300,7 @@ describe('Test update user profile. ', function () {
      * Test update messageBlackList with invalid data.
      */
     it('Test update messageBlackList', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ messageBlackList: '["Hung", "user", "aa"]'})
             .end(function (err, resp) {
                 assertError(err, resp, "messageBlackList contains invalid user handle.", done);
@@ -313,9 +311,9 @@ describe('Test update user profile. ', function () {
      * Test update receiveMessages.
      */
     it('Test update receiveMessages', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ receiveMessages: 'yes'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -324,7 +322,7 @@ describe('Test update user profile. ', function () {
      * Test update receiveMessages with invalid data.
      */
     it('Test update receiveMessages', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ receiveMessages: 'a'})
             .end(function (err, resp) {
                 assertError(err, resp, "receiveMessages value is invalid.", done);
@@ -335,9 +333,9 @@ describe('Test update user profile. ', function () {
      * Test update showMyEarnings.
      */
     it('Test update showMyEarnings', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ showMyEarnings: 'show'})
-            .end(function (err, resp) {
+            .end(function () {
                 done();
             });
     });
@@ -346,7 +344,7 @@ describe('Test update user profile. ', function () {
      * Test update showMyEarnings with invalid data.
      */
     it('Test update showMyEarnings', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ showMyEarnings: 'a'})
             .end(function (err, resp) {
                 assertError(err, resp, "showMyEarnings value is invalid.", done);
@@ -357,7 +355,7 @@ describe('Test update user profile. ', function () {
      * Test update showMySchool with invalid data.
      */
     it('Test update showMySchool', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 400);
+        var req = getRequest('/v2/user/profile', user124916, 400);
         req.send({ showMySchool: 'yes'})
             .end(function (err, resp) {
                 assertError(err, resp, "showMySchool value is not allowed to update for this user.", done);
@@ -368,14 +366,14 @@ describe('Test update user profile. ', function () {
      * Test get actions.
      */
     it('Test get actions', function (done) {
-        var req = getRequest('/v2/user/profile', user132456, 200);
+        var req = getRequest('/v2/user/profile', user124916, 200);
         req.send({ showMyEarnings: 'show', receiveMessages: 'yes', messageBlackList: '["Hung", "user"]',
             emailNotification: '{ "Algorithm Competitions": "Yes"}'})
-            .end(function (err, resp) {
+            .end(function () {
                 var req2 = request(API_ENDPOINT)
                     .get('/v2/user/profile')
                     .set('Accept', 'application/json')
-                    .set('Authorization', getAuthHeader(user132456))
+                    .set('Authorization', getAuthHeader(user124916))
                     .expect('Content-Type', /json/)
                     .expect(200);
 
