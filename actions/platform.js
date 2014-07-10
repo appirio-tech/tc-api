5695c1afa22da2dc4d7557fa3e93d9cbac4b9b16
Shannon Ma <sma@appirio.com>
2014-07-10 15:58:33 -0500

    Fix.

--- actions/platform.js
+++ actions/platform.js
@@ -363,7 +363,7 @@ function billingAccountsPermission(api, connection, next) {
             api.dataAccess.executeQuery("get_user_accounts", { users: us }, dbConnectionMap, cb);
         }, function (res, cb) {
             userAccountIds = _.pluck(res, "user_account_id");
-            newToUserAccount = _.difference(existHandle, _.pluck(res, "handle_lower"));
+            newToUserAccount = _.difference(us, _.pluck(res, "handle_lower"));
 
             // insert user into user_account table
             // insert dummy address contact also.
