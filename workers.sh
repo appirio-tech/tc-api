f0395adb83de90735fbae0afb1244dc2b7c6712e
Thabo <tha.bo.fletcher@gmail.com>
2015-04-09 16:58:07 -0700

    start two workers by default, cleanup startup scripts, and allow reload/add/rm workers

---
+++ workers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ACTION=$1
+SIGNAL=""
+
+if [ "$ACTION" == "reload" ]; then
+	SIGNAL="HUP"
+elif [ "$ACTION" == "add" ]; then
+	SIGNAL="TTIN"
+elif [ "$ACTION" == "rm" ]; then
+	SIGNAL="TTOU"
+fi
+
+if [ "$SIGNAL" == "" ]; then
+	echo "Usage: workers.sh [reload|add|rm]"
+else
+	pid=$(ps ax | grep startCluster | grep -v grep | cut -f1 -d' ')
+	if [ "$pid" == "" ]; then echo "No cluster master process found."
+	else
+		kill -$SIGNAL $pid
+		echo "Signal $SIGNAL sent to process: $pid" 
+	fi
+fi
