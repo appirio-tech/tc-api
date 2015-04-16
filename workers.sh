3258c14983cc405e0c516b0fa0eb09c71a5001c7
Thabo <tha.bo.fletcher@gmail.com>
2015-04-16 16:50:58 -0700

    add better clustering commands

--- workers.sh
+++ workers.sh
@@ -2,21 +2,90 @@
 ACTION=$1
 SIGNAL=""
 
+pid=$(ps ax | grep startCluster | grep -v grep | xargs | cut -f1 -d' ')
+if [ "$pid" == "" ]; then 
+	echo "No cluster master process found."
+	exit
+fi
+
+function workers_ps {
+	ps_out=$(ps ax | grep -- "actionhero start$" | grep -v grep)
+	IFS='
+	'
+	IFS=${IFS:0:1}
+	workers=( $ps_out )
+	}
+
+workers_ps
+
+wpids=()
+for worker in "${workers[@]}"
+do
+	wpids+=("$(echo -e "${worker}" | xargs | cut -f1 -d' ')")
+done
+
+
 if [ "$ACTION" == "reload" ]; then
 	SIGNAL="HUP"
 elif [ "$ACTION" == "add" ]; then
 	SIGNAL="TTIN"
 elif [ "$ACTION" == "rm" ]; then
 	SIGNAL="TTOU"
+elif [ "$ACTION" == "kill" ]; then
+	for wpid in "${wpids[@]}"
+	do
+        kill -9 $wpid # kill the workers and let master restart
+	done
+	exit
+elif [ "$ACTION" == "recycle" ]; then
+	numWorkers=${#wpids[@]}
+	echo "Start Recycle, Workers Running: ${#workers[@]}"
+
+	for wpid in "${wpids[@]}"
+	do
+        kill -TTIN $pid # add one
+        sleep 1
+	done
+
+	declare -i sleepTime
+	sleepTime=$numWorkers
+	sleep $sleepTime
+	
+	workers_ps
+	echo "Max Workers Running: ${#workers[@]}"
+
+	# signal recycle of all
+	kill -HUP $pid
+
+	sleepTime=$numWorkers*2
+	sleep $sleepTime
+
+	for wpid in "${wpids[@]}"
+	do
+        kill -TTOU $pid # remove one
+        sleep 1
+	done
+
+	sleepTime=$numWorkers/2
+	sleep $sleepTime
+
+	workers_ps
+	echo "End Recycle, Workers Running: ${#workers[@]}"
+	exit
+elif [ "$ACTION" == "ls" ]; then
+	for worker in "${workers[@]}"
+	do
+		echo -e "${worker}" | xargs | cut -f1,4 -d' '
+	done
+	echo "Count: ${#wpids[@]}" 
+	exit
+elif [ "$ACTION" == "count" ]; then
+    echo "${#wpids[@]}"
+	exit
+else
+	echo "Usage: workers.sh [reload|recycle|add|rm|ls|count]"
+	exit
 fi
 
-if [ "$SIGNAL" == "" ]; then
-	echo "Usage: workers.sh [reload|add|rm]"
-else
-	pid=$(ps ax | grep startCluster | grep -v grep | xargs | cut -f1 -d' ')
-	if [ "$pid" == "" ]; then echo "No cluster master process found."
-	else
-		kill -$SIGNAL $pid
-		echo "Signal $SIGNAL sent to process: $pid" 
-	fi
-fi
+kill -$SIGNAL $pid
+echo "Signal $SIGNAL sent to process: $pid" 
