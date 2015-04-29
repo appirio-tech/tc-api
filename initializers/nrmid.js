057cb3172cf5c1fdc6dc547ad6843d316ff9d341
Thabo <tha.bo.fletcher@gmail.com>
2015-04-29 13:15:01 -0700

    show a warning when newrelic is not present/configured

--- initializers/nrmid.js
+++ initializers/nrmid.js
@@ -1,19 +1,24 @@
-try {
-    var newrelic = require("newrelic");
-} catch (ignore) { }
+var newrelic = false;
 
 var fixTransactionName = function(connection, actionTemplate, next) {
   if(newrelic && connection.type === 'web'){
     newrelic.setControllerName(actionTemplate.name);
   }
   next(connection, true);
-}
+};
 
 var reportException = function(type, err, extraMessages, severity){
   if(newrelic) newrelic.noticeError(err);
-}
+};
 
 exports.nrmid = function(api, next){
+  try {
+    newrelic = require('newrelic');
+  } catch (error) { 
+    api.log('Newrelic could not be initialized: ' + error, 'warning');
+    newrelic = false;
+  }
+
   if(newrelic) {
     api.actions.preProcessors.push(fixTransactionName);
     api.exceptionHandlers.reporters.push(reportException);
