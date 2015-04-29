3972fe875ba6da7f5dedecac4b6b7a1ce8832879
Thabo <tha.bo.fletcher@gmail.com>
2015-04-29 13:16:28 -0700

    restore unclustered operation by default

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # nohup node ~/tc-api/node_modules/.bin/actionHero start 2>&1 &
-#forever start -d -v -a -l "${DIR}/log/forever.log" "${DIR}/node_modules/actionhero/bin/actionhero" start
-forever start -d -a -l "${DIR}/log/forever.log" "${DIR}/node_modules/actionhero/bin/actionhero" startCluster --workers=10
+forever start -d -v -a -l "${DIR}/log/forever.log" "${DIR}/node_modules/actionhero/bin/actionhero" start
+#forever start -d -a -l "${DIR}/log/forever.log" "${DIR}/node_modules/actionhero/bin/actionhero" startCluster --workers=10
