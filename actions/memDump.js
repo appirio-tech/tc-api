b33fa3b3837c62e49bc78a5a133ccaf2b0c9fd14
Thabo <tha.bo.fletcher@gmail.com>
2015-07-11 14:15:44 -0700

    cleanup

--- actions/memDump.js
+++ actions/memDump.js
@@ -1,21 +1,8 @@
 /*
  * Copyright (C) 2014 TopCoder Inc., All Rights Reserved.
- *
- * @version 1.1
- * @author Ghost_141
- * Changes in 1.1:
- * - Add technologies and platforms filter.
  */
 "use strict";
 var heapdump = require('heapdump');
-
-var async = require('async');
-var _ = require('underscore');
-var fs = require('fs');
-var BadRequestError = require('../errors/BadRequestError');
-var UnauthorizedError = require('../errors/UnauthorizedError');
-var ForbiddenError = require('../errors/ForbiddenError');
-
 
 exports.dumpMemory = {
     name: 'dumpMemory',
@@ -24,14 +11,7 @@ exports.dumpMemory = {
         required: [],
         optional: []
     },
-    // blockedConnectionTypes: [],
-    //outputExample: {},
     version: 'v2',
-    // transaction: 'read',
-    // databases: [],
-    // outputExample: {
-    //     string: "hello"
-    //   },
     run: function (api, connection, next) {
         if (process.env.ADMIN_API_KEY && connection.params.apiKey === process.env.ADMIN_API_KEY) {
             heapdump.writeSnapshot(function(err, filename) {
